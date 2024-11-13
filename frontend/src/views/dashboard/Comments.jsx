c86ee1e6f134dcbba6560c627fbaebed41320ac2
m-stavrakov <131870370+m-stavrakov@users.noreply.github.com>
2024-11-13 16:44:02 +0000

    Comment page and reply functionality added

--- frontend/src/views/dashboard/Comments.jsx
+++ frontend/src/views/dashboard/Comments.jsx
@@ -2,8 +2,44 @@ import React from "react";
 import Header from "../partials/Header";
 import Footer from "../partials/Footer";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import apiInstance from "../../utils/axios";
+import useUserData from "../../plugin/useUserData";
+import Moment from "../../plugin/Moment";
+import moment from "moment";
+import { useEffect } from "react";
+import Toast from "../../plugin/Toast";
 
 function Comments() {
+
+    const [comments, setComment] = useState([]);
+    const [reply, setReply] = useState("");
+    const user_id = useUserData()?.user_id;
+
+    const fetchComments = async () => {
+        const response = await apiInstance.get(`author/dashboard/comment-list/${user_id}/`);
+        setComment(response?.data);
+    };
+
+    useEffect(() => {
+        fetchComments();
+    }, []);
+
+    const handleSubmitReply = async (commentId) => {
+        try {
+            const response = await apiInstance.post(`author/dashboard/reply-comment/`, {
+                comment_id: commentId,
+                reply: reply,
+            });
+            console.log(response.data);
+            fetchComments();
+            Toast("success", "Reply sent");
+            setReply("");
+        } catch (error) {
+            console.log(error);
+        };
+    };
+
     return (
         <>
             <Header />
@@ -25,59 +61,57 @@ function Comments() {
                                     {/* List group */}
                                     <ul className="list-group list-group-flush">
                                         {/* List group item */}
-                                        <li className="list-group-item p-4 shadow rounded-3">
-                                            <div className="d-flex">
-                                                <img src="https://geeksui.codescandy.com/geeks/assets/images/avatar/avatar-1.jpg" alt="avatar" className="rounded-circle avatar-lg" style={{ width: "70px", height: "70px", borderRadius: "50%", objectFit: "cover" }} />
-                                                <div className="ms-3 mt-2">
-                                                    <div className="d-flex align-items-center justify-content-between">
-                                                        <div>
-                                                            <h4 className="mb-0">Eleanor Pena</h4>
-                                                            <span>2 hour ago</span>
+                                        {comments.map((c, index) => (
+                                            <li key={index} className="list-group-item p-4 shadow rounded-3">
+                                                <div className="d-flex">
+                                                    {/* <img src="https://geeksui.codescandy.com/geeks/assets/images/avatar/avatar-1.jpg" alt="avatar" className="rounded-circle avatar-lg" style={{ width: "70px", height: "70px", borderRadius: "50%", objectFit: "cover" }} /> */}
+                                                    <div className="ms-3 mt-2">
+                                                        <div className="d-flex align-items-center justify-content-between">
+                                                            <div>
+                                                                <h4 className="mb-0">{c?.name}</h4>
+                                                                <span>{Moment(c?.date)}</span>
+                                                            </div>
                                                         </div>
-                                                        <div>
-                                                            <a href="#" data-bs-toggle="tooltip" data-placement="top" title="Report Abuse">
-                                                                <i className="fe fe-flag" />
-                                                            </a>
-                                                        </div>
-                                                    </div>
-                                                    <div className="mt-2">
-                                                        <p className="mt-2">
-                                                            <span className="fw-bold me-2">
-                                                                Comment <i className="fas fa-arrow-right"></i>
-                                                            </span>
-                                                            This post was really amazing, do you recommend that beginners learn React.js and Django?
-                                                        </p>
-                                                        <p className="mt-2">
-                                                            <span className="fw-bold me-2">
-                                                                Response <i className="fas fa-arrow-right"></i>
-                                                            </span>
-                                                            Thanks for the commment. Yes, it's an ideal stack for proficient development.
-                                                        </p>
-                                                        <p>
-                                                            <button class="btn btn-outline-secondary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                                                                Send Response
-                                                            </button>
-                                                        </p>
-                                                        <div class="collapse" id="collapseExample">
-                                                            <div class="card card-body">
-                                                                <form>
-                                                                    <div class="mb-3">
-                                                                        <label for="exampleInputEmail1" class="form-label">
-                                                                            Write Response
-                                                                        </label>
-                                                                        <textarea name="" id="" cols="30" className="form-control" rows="4"></textarea>
+                                                        <div className="mt-2">
+                                                            <p className="mt-2">
+                                                                <span className="fw-bold me-2">
+                                                                    Comment <i className="fas fa-arrow-right"></i>
+                                                                </span>
+                                                                {c?.comment}
+                                                            </p>
+                                                            <p className="mt-2">
+                                                                <span className="fw-bold me-2">
+                                                                    Response <i className="fas fa-arrow-right"></i>
+                                                                </span>
+                                                                {c?.reply || "No reply"}
+                                                            </p>
+                                                            <p>
+                                                                {/* without ${c.id.toString()} all collapses will open, this makes it specific so only teh one you click on will open */}
+                                                                <button class="btn btn-outline-secondary" type="button" data-bs-toggle="collapse" data-bs-target={`#collapseExample${c.id.toString()}`} aria-expanded="false" aria-controls={`collapseExample${c.id.toString()}`}>
+                                                                    Send Response
+                                                                </button>
+                                                            </p>
+                                                            <div class="collapse" id={`collapseExample${c.id.toString()}`}>
+                                                                <div class="card card-body">
+                                                                    <div>
+                                                                        <div class="mb-3">
+                                                                            <label for="exampleInputEmail1" class="form-label">
+                                                                                Write Response
+                                                                            </label>
+                                                                            <textarea onChange={(e) => setReply(e.target.value)} value={reply} name="" id="" cols="30" className="form-control" rows="4"></textarea>
+                                                                        </div>
+    
+                                                                        <button onClick={() => handleSubmitReply(c.id)} type="submit" class="btn btn-primary">
+                                                                            Send Response <i className="fas fa-paper-plane"> </i>
+                                                                        </button>
                                                                     </div>
-
-                                                                    <button type="submit" class="btn btn-primary">
-                                                                        Send Response <i className="fas fa-paper-plane"> </i>
-                                                                    </button>
-                                                                </form>
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                        </li>
+                                            </li>
+                                        ))}
                                     </ul>
                                 </div>
                             </div>
